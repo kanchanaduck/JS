5396682ad578f4c41f360ade620745487c1c3232
014496 <014496@CPT-2019-P01259>
2022-05-23 10:49:03 +0700

    Production

--- JKSSYSTEM/src/environments/environment.prod.ts
+++ JKSSYSTEM/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apijks: 'http://cptsvs52t/api_jks/api'
+  // apijks: 'http://cptsvs52t/api_jks/api',
+  apijks: 'http://cptsvs531:5000/api-jks/api',
 };
--- JKSSYSTEM/src/environments/environment.ts
+++ JKSSYSTEM/src/environments/environment.ts
@@ -5,13 +5,14 @@
 
 export const environment = {
   production: false,
-  apijks: 'https://localhost:5001/api',
+  // apijks: 'https://localhost:5001/api',
   // apijks: 'http://cptsvs52t/api_jks/api',
+  apijks: 'http://cptsvs531:5000/api-jks/api',
   api: 'http://cptsvs531:5000/api-check-sheet/api',
   //api_euc: 'http://cptsvs531:1000/middleware/oracle/euc', not ยูส
 };
 
-// apijks: 'http://cptsvs531/api_jks/api',
+// apijks: 'http://cptsvs531:5000/api_jks/api',
 // apijks: 'http://cptsvs52t/api_jks/api', //test
 // apijks: 'https://localhost:5001/api',
 //ng build --base-href=/jkssystem/    /// ระบุชื่อโฟลเดอร์ ให้ตรงกับบน Server ที่จะวาง
